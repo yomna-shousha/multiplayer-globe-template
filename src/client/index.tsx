74ce044f4e5e0fb758da63bb53d940f7eb3ef5b2
Yomna Shousha <yomna@cloudflare.com>
2025-10-29 21:35:08 -0400

    Change header text from 'Where's everyone at?' to 'right here lol?'

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -90,7 +90,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Where's everyone at?</h1>
+      <h1>right here lol?</h1>
       {counter !== 0 ? (
         <p>
           <b>{counter}</b> {counter === 1 ? "person" : "people"} connected.
